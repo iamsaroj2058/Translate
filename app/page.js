1c1721b18cb1e08e83c7f3e128ad0ba4d31c5160
pknowledge <crackit441@gmail.com>
2024-02-14 23:25:29 +0545

    first commit

--- app/page.js
+++ app/page.js
@@ -1,18 +1,40 @@
+
+'use client'
 import Image from "next/image";
 import Header from "@/components/Header";
 import Menu from "../components/Menu"
 import menuData from "./data";
+import React, { useEffect } from 'react';
+
 
 
 export default function Home() {
-  console.log(menuData);
+  useEffect(() => {
+    if (!window.googleTranslateElementInit) {
+    // Load the Google Translate API script dynamically
+    const script = document.createElement('script');
+    script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+    script.async = true;
+    document.head.appendChild(script);
+
+    // Define the googleTranslateElementInit function
+    window.googleTranslateElementInit = () => {
+      new window.google.translate.TranslateElement(
+        { pageLanguage: 'en' },
+        'google_translate_element'
+      );
+    }; }
+  }, []);
 
   return (
     <div> 
       <Header/>
      <Image/>
+     <div id="google_translate_element"></div>
+
      <Menu data={menuData}/>
      
+    
     </div>
   
   );
